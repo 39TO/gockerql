c2d39de78e72dc73b0b6ef69dd23ec45a02dc7b0
39TO <taiyo.murase39@gmail.com>
2023-05-27 18:46:20 +0900

    fix: docker

---
+++ server/database/sql/1_create_player_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `players` {
+    `id`              varchar(255) COLLATE utf8mb4_bin NOT NULL,
+    `name`            varchar(255) COLLATE utf8mb4_bin NOT NULL,
+}
---
+++ server/database/sql/2_insert_player.sql
@@ -0,0 +1,3 @@
+INSERT INTO rooms (`id`, `name`) VALUES ("c1", "パトリック");
+INSERT INTO rooms (`id`, `name`) VALUES ("c2", "木下康介");
+
